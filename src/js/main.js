c0ab0bf6cb6ba56bb58e71fb1253a22aa8a36508
Oluwatoni <odulenulerrn6@gmail.com>
2024-02-01 12:47:43 +0100

    Contact section markup

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
 // CHANGE BACKGROUND HEADER
 function scrollHeader() {
-  const header = document.getElementById('header');
+  const header = document.getElementById("header");
   //When the scroll is greater than 50 viewport height add the 'scroll-header' class to the header tag.
   if (this.scrollY >= 50) {
-    header.classList.add('scroll-header');
+    header.classList.add("scroll-header");
   } else {
-    header.classList.remove('scroll-header');
+    header.classList.remove("scroll-header");
   }
 }
 
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 // SWIPER POPULAR
 var swiperPopular = new Swiper(".popular__container", {
@@ -17,7 +17,7 @@ var swiperPopular = new Swiper(".popular__container", {
     spaceBetween: 32,
     grabCursor: true,
     centeredSlides: true,
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     loop: true,
 
     nextEl: ".swiper-button-next",
@@ -26,13 +26,31 @@ var swiperPopular = new Swiper(".popular__container", {
 });
 
 // VALUE ACCORDION
-const accordionItems = document.querySelectorAll('.value__accordion-item');
+const accordionItems = document.querySelectorAll(".value__accordion-item");
 
-accordionItems.forEach(item => {
-  const itemHeader = item.querySelector('header');
-  itemHeader.addEventListener('click', event => {
-    const itemContent = item.querySelector('.value__accordion-content');
+accordionItems.forEach((item) => {
+  const itemHeader = item.querySelector("header");
 
-    itemContent.style.height = (itemContent.style.height === '0px' || itemContent.style.height === '') ? 'auto' : '0';
-  })
-})
+  itemHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+
+});
+
+const toggleItem = (item) => {
+  const itemContent = item.querySelector('.value__accordion-content');
+
+  if (item.classList.contains('accordion-open')) {
+    itemContent.removeAttribute('style');
+    item.classList.remove('accordion-open')
+  } else {
+    itemContent.style.height = itemContent.scrollHeight + "px";
+    item.classList.add('accordion-open');
+  }
+}
