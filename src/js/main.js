4cd87fdaf7ef062cf7ed1a9b5f89d383deb417cd
Oluwatoni <odulenulerrn6@gmail.com>
2024-02-09 11:47:58 +0100

    Dark theme

--- src/js/main.js
+++ src/js/main.js
@@ -54,3 +54,73 @@ const toggleItem = (item) => {
     item.classList.add('accordion-open');
   }
 }
+
+//SCROLL TO SECTIONS USING ACTIVE LINKS
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 58,
+          sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active-link')
+    } else {
+      document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive);
+
+// SHOW SCROLL UP BUTTON
+function scrollUp(){
+  const scrollUpBtn = document.getElementById('scroll-up');
+  //When scroll is higher than 350vh, display scrollup button
+
+  if(this.scrollY >= 350 ) {
+    scrollUpBtn.classList.add('show-scroll')
+  } else {
+    scrollUpBtn.classList.remove('show-scroll');
+  }
+}
+
+window.addEventListener('scroll', scrollUp);
+
+// DARK LIGHT THEME
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun'
+
+//Previously selected topic (if user selected one)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+//We obtain the current theme that the interface has by validating the dark theme class
+const getCurrentTheme = () => {
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+}
+
+const getCurrentIcon = () => {
+  themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+}
+
+//We validate if the user previously chose a topic
+if (selectedTheme) {
+  //If the validation was fulfilled, we ask the issue was to know if we activated or deactivated the dark theme
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+};
+
+//Activate or deactivate the theme manually
+themeButton.addEventListener('click', ()=> {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  //Save the current theme and icon
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+})
